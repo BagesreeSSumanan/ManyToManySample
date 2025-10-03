b51200d02e28f78101fddefce69b886b37d37eae
Bagesree S Sumanan <bagesree.s.sumanan@xminds.com>
2025-10-03 16:18:17 +0530

    insert

--- src/app.js
+++ src/app.js
@@ -2,9 +2,51 @@ const express = require('express');
 const config = require('./config/config');
 const app = express();
 require('dotenv').config();
+const {createTutorial,createTag,addTutorial}= require('./controller/controller');
 app.use(express.json());
+const tutorialRouter = express.Router();
 
 const PORT = config.port;
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`);
 });
+app.use('/api', tutorialRouter);
+  tutorialRouter.post('/createTutorial',  async (req, res, next)=>{
+     try{
+         
+        if (!req.body) {
+            return res.sendStatus(400);
+        }
+         const NewTutorial =  await createTutorial(req.body).then(() => res.json({ message: 'Tutorial created.' }));
+ 
+     } catch(e){
+         console.log(e);
+         res.sendStatus(400);
+     }
+  });
+    tutorialRouter.post('/createTag',  async (req, res, next)=>{
+     try{
+         
+        if (!req.body) {
+            return res.sendStatus(400);
+        }
+         const NewTag=  await createTag(req.body).then(() => res.json({ message: 'Tag created.' }));
+ 
+     } catch(e){
+         console.log(e);
+         res.sendStatus(400);
+     }
+  });
+tutorialRouter.post('/addTutorial',  async (req, res, next)=>{
+     try{
+         
+        if (!req.body) {
+            return res.sendStatus(400);
+        }
+         const NewTag=  await addTutorial(req.body.tag,req.body.tutorial).then(() => res.json({ message: 'Tag created.' }));
+ 
+     } catch(e){
+         console.log(e);
+         res.sendStatus(400);
+     }
+  });
---
+++ src/controller/controller.js
@@ -0,0 +1,55 @@
+const { where } = require('sequelize');
+const db = require('../config/db');
+const Tutorial = db.tutorial;
+const Tag = db.tag;
+
+const createTutorial = (tutorial) => {
+  return Tutorial.create({
+    title: tutorial.title,
+    description: tutorial.description,
+  })
+    .then((tutorial) => {
+      console.log(">> Created tutorial: " + JSON.stringify(tutorial, null, 4));
+      return tutorial;
+    })
+    .catch((err) => {
+      console.log(">> Error while creating tutorial: ", err);
+    });
+};
+
+const createTag = (tag) => {
+  return Tag.create({
+    name: tag.name,
+  })
+    .then((tag) => {
+      console.log(">> Created Tag: " + JSON.stringify(tag, null, 2));
+      return tag;
+    })
+    .catch((err) => {
+      console.log(">> Error while creating Tag: ", err);
+    });
+};
+
+const addTutorial = (tagId, tutorialId) => {
+  return Tag.findByPk(tagId)
+    .then((tag) => {
+      if (!tag) {
+        console.log("Tag not found!");
+        return null;
+      }
+      return Tutorial.findByPk(tutorialId).then((tutorial) => {
+        if (!tutorial) {
+          console.log("Tutorial not found!");
+          return null;
+        }
+
+        tag.addTutorial(tutorial);
+        console.log(`>> added Tutorial id=${tutorial.id} to Tag id=${tag.id}`);
+        return tag;
+      });
+    })
+    .catch((err) => {
+      console.log(">> Error while adding Tutorial to Tag: ", err);
+    });
+};
+module.exports = { createTutorial,createTag,addTutorial};
